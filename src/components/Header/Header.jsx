e993c3e41b8909d27712de185d0c4ede47b21af2
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-09-11 21:27:28 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/image/profile.png'
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between p-5 mx-8 items-center border-b-2'>
             <h1 className='text-5xl'>React Knowledge Cafe </h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
